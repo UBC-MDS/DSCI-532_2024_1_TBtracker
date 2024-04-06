3d81ae1338d1a5846f8b65e972e258c3d862f7e8
sean-m-mckay <144716905+sean-m-mckay@users.noreply.github.com>
2024-04-06 14:50:52 -0700

    Server setup (#43)
    * Server setup
    
    * Cut down requriements
    
    * Update requirements.txt with plotly

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ import dash_bootstrap_components as dbc
 
 # Initialize Dash app
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
+server = app.server
 app.layout = main_layout
 
 # Check if the file is executed as main (useful for running the server from this file if needed)
 if __name__ == "__main__":
-    app.run_server(port=8000, host="127.0.0.1",
-                   debug=True, dev_tools_hot_reload=True)
+    app.run(port=8000, host="127.0.0.1",
+                   debug=False, dev_tools_hot_reload=False)
+    
