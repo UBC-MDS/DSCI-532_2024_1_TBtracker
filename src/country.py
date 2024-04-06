2bfbb4f7b2c96da22c132654940dd5220524de8a
Yiwei Zhang <zywkloo@student.ubc.ca>
2024-04-06 02:35:21 -0700

    Finished file restructure

--- src/country.py
+++ src/country.py
@@ -1,4 +1,4 @@
-from dash import dcc
+from dash import dcc, Input, Output, callback
 import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
@@ -40,11 +40,40 @@ country_page = dbc.Container(
     ]
 )
 
-# # This has to be done in a separate callback than below
-# # Otherwise the rf-country-dropdown is not yet defined before we switch tabs
-# @callback(
-#     Output('rf-country-dropdown', 'value'),
-#     Input('memory-output', 'data')
-# )
-# def update_dropdown(data):
-#     return data
+
+@callback(
+    Output('indicator-graphic', 'figure'),
+    Input('rf-country-dropdown', 'value'),
+    Input('rf-sex-dropdown', 'value'),
+    Input('rf-age-dropdown', 'value'))
+def update_graph(country_value, xaxis_sex, xaxis_age):
+    rff = rf_data[rf_data['country'] == country_value]
+    rff1 = rff.groupby(['age_group', 'sex'], as_index=False)['best'].sum()
+
+    if xaxis_age == None:
+        rff2 = rff1.copy()
+    else:
+        rff2 = rff1.loc[rff1["age_group"].isin(xaxis_age)]
+
+    if xaxis_sex == None:
+        rff3 = rff2.copy()
+    else:
+        rff3 = rff2.loc[rff2["sex"] == xaxis_sex]
+
+    fig = px.bar(rff3, x='age_group', y='best', color='sex',
+                 labels={'age_group': 'Age Group',
+                         'best': 'TB Incidence (Estimate from WHO)'})
+
+    fig.update_layout(
+        margin={'l': 40, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
+
+    # Custom legend labels
+    custom_labels = {'m': 'Male', 'f': 'Female'}
+
+    for trace in fig.data:
+        if trace.name in custom_labels:
+            trace.name = custom_labels[trace.name]
+
+    fig.update_layout(legend_title_text='Sex')
+
+    return fig
