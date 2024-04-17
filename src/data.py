3c2af5361feaa0e146aa8734792e0add3679eb6c
Yiwei Zhang <zywkloo@student.ubc.ca>
2024-04-16 22:20:23 -0700

    Rename Dir

--- src/data.py
+++ src/data.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 # Load the preprocessed data once at the start to avoid reloading it on each callback.
 preprocessed_rf_data = pd.read_parquet(
-    "data/preprocessing/rf_type_data.parquet")
-rf_data = pd.read_parquet("data/preprocessing/rf_data.parquet")
-tb_data = pd.read_parquet("data/preprocessing/tb_data.parquet")
+    "data/processed/rf_type_data.parquet")
+rf_data = pd.read_parquet("data/processed/rf_data.parquet")
+tb_data = pd.read_parquet("data/processed/tb_data.parquet")
