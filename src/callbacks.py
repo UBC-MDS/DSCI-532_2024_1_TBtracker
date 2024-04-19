1b62af3f0f607b596b2df0c2de35763bc746ac2a
HinaBandukwala <hnbandukwala@gmail.com>
2024-04-18 17:28:02 -0700

    added improved logic for relative nums in card (#123)

--- src/callbacks.py
+++ src/callbacks.py
@@ -51,53 +51,58 @@ def update_card(selected_year, selected_type, selected_value):
 def update_global_stats(selected_year, selected_type, selected_value):
     y_column_mapping = {
         ("absolute", "incidence"): "incidence_total",
-        ("relative", "incidence"): "incidence_rate",
+        ("relative", "incidence"): "incidence_total",
         ("absolute", "mortality"): "mortality_total",
-        ("relative", "mortality"): "mortality_rate",
+        ("relative", "mortality"): "mortality_total",
     }
     y_column = y_column_mapping.get((selected_type, selected_value), "incidence_total")
 
-    tb_data["year_dt"] = pd.to_datetime(tb_data["year"], format="%Y")
+    tb_data["year_dt"] = pd.to_datetime(tb_data["year"], format='%Y')
     selected_year_dt = pd.Timestamp(str(selected_year))
     previous_year_dt = selected_year_dt - pd.DateOffset(years=1)
     next_year_dt = selected_year_dt + pd.DateOffset(years=1)
 
-    global_stat = tb_data.loc[tb_data["year"] == selected_year, y_column].sum()
+    if selected_type == "relative":
+        global_stat = (tb_data.loc[tb_data["year"] == selected_year][y_column].sum() / tb_data.loc[tb_data["year"] == selected_year]["e_pop_num"].sum()) * 100 
+    else:
+        global_stat = tb_data.loc[tb_data["year"] == selected_year, y_column].sum()
+
 
     diff_previous = diff_next = None  # Default values for differences
-    diff_previous_color = diff_next_color = "black"  # Default text color
+    diff_previous_color = diff_next_color = 'black'  # Default text color
 
     if selected_year != 2000:
-        global_stat_previous = tb_data.loc[
-            tb_data["year_dt"] == previous_year_dt, y_column
-        ].sum()
-        if global_stat_previous:
-            diff_previous = round(
-                ((global_stat - global_stat_previous) / global_stat_previous) * 100, 1
-            )
-            diff_previous_color = "blue" if diff_previous > 0 else "red"
-
+        if selected_type == "absolute":
+            global_stat_previous = tb_data.loc[tb_data["year_dt"] == previous_year_dt, y_column].sum()
+            if global_stat_previous:
+                diff_previous = ((global_stat - global_stat_previous) / global_stat_previous) * 100
+                diff_previous_color = "blue" if diff_previous > 0 else "red"
+        else:
+            global_stat_previous = (tb_data.loc[tb_data["year_dt"] == previous_year_dt, y_column].sum() / tb_data.loc[tb_data["year_dt"] == previous_year_dt, 'e_pop_num'].sum()) * 100
+            if global_stat_previous:
+                diff_previous = global_stat - global_stat_previous
+                diff_previous_color = "blue" if diff_previous > 0 else "red"
+    
     if selected_year != 2022:
-        global_stat_next = tb_data.loc[
-            tb_data["year_dt"] == next_year_dt, y_column
-        ].sum()
-        if global_stat_next:
-            diff_next = round(
-                ((global_stat - global_stat_next) / global_stat_next) * 100, 1
-            )
-            diff_next_color = "blue" if diff_next > 0 else "red"
+        if selected_type == "absolute":
+            global_stat_next = tb_data.loc[tb_data["year_dt"] == next_year_dt, y_column].sum()
+            if global_stat_next:
+                diff_next = ((global_stat - global_stat_next) / global_stat_next) * 100 
+                diff_next_color = "blue" if diff_next > 0 else "red"
+        else:
+            global_stat_next = (tb_data.loc[tb_data["year_dt"] == next_year_dt, y_column].sum() / tb_data.loc[tb_data["year_dt"] == next_year_dt, 'e_pop_num'].sum()) * 100
+            if global_stat_next:
+                diff_next = global_stat - global_stat_next
+                diff_next_color = "blue" if diff_next > 0 else "red"
 
-    diff_previous_text = (
-        f"{diff_previous:+.1f}%" if diff_previous is not None else "data not available"
-    )
-    diff_next_text = (
-        f"{diff_next:+.1f}%" if diff_next is not None else "data not available"
-    )
-
-    if selected_type != "absolute":
-        global_stat = f"{global_stat:.2f}"
+    if selected_type == "absolute":
+        global_stat = f"{global_stat:.0f}"
+        diff_previous_text = f"{diff_previous:+.1f}%" if diff_previous is not None else "data not available"
+        diff_next_text = f"{diff_next:+.1f}%" if diff_next is not None else "data not available"
     else:
-        global_stat = round(global_stat)
+        global_stat = f"{global_stat:.5f}%"
+        diff_previous_text = f"{diff_previous:+.5f}%" if diff_previous is not None else "data not available"
+        diff_next_text = f"{diff_next:+.5f}%" if diff_next is not None else "data not available"
 
     return (
         global_stat,
